d327c46fca5945e0a8808590e741f4331926f89e
Максим Сухарев <maxs130872@gmail.com>
2022-05-08 17:27:01 +0700

    Создание структур для реализации бизнес-логики приложения

---
+++ src/ContactsApp.Models/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace ContactsApp.Models;
+
+using System.Collections.Generic;
+
+/// <summary>
+///     Класс проекта телефонного справочника
+/// </summary>
+public class Project
+{
+    /// <summary>
+    ///     Список контактов справочника
+    /// </summary>
+    public List<Contact>? Contacts { get; set; }
+}
