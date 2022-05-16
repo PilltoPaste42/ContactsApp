4e7dc63a57b40f12d5a319ea6d622dacba493b88
SukharevMA <maxs130872@gmail.com>
2022-05-17 05:05:38 +0700

    Создание делигата

--- src/ContactsApp.View/App.xaml.cs
+++ src/ContactsApp.View/App.xaml.cs
@@ -1,11 +1,13 @@
-﻿namespace ContactsApp.View
-{
-    using System.Windows;
+﻿namespace ContactsApp.View;
 
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+using System.Windows;
+
+using ContactsApp.Models;
+
+/// <summary>
+///   Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public delegate void AddContactDelegate(Contact contact);
 }
