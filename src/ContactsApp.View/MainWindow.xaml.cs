4897cfbc48a93750548ecf0678b9a51679491755
PilltoPaste42 <50257225+PilltoPaste42@users.noreply.github.com>
2022-05-26 01:05:27 +0700

    Features/2 add forms layout (#1)
    * Создание решения и проектов
    
    * Создание окон приложения
    
    * Рефакторинг всей реализации
    
    * Исправление вызова новых окон
    
    Co-authored-by: PilltoPaste42 <5892_sma@kcup.net>

---
+++ src/ContactsApp.View/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace ContactsApp.View
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddContactButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            CreateAddContactForm();
+        }
+
+        private Window CreateAddContactForm()
+        {
+            var window = new AddContactWindow
+            {
+                Owner = this
+            };
+            window.Show();
+
+            return window;
+        }
+
+        private void MenuEditAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            CreateAddContactForm();
+        }
+
+        private void MenuFileExit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuHelpAbout_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = new AboutAppWindow
+            {
+                Owner = this
+            };
+            window.Show();
+        }
+    }
+}
