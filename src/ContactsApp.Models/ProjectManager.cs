c532492d538c8eda32b302d0a735ba8292979931
PilltoPaste42 <50257225+PilltoPaste42@users.noreply.github.com>
2022-05-26 01:26:01 +0700

    Features/3 add data structures (#2)
    * Создание решения и проектов
    
    * Создание структур для реализации бизнес-логики приложения
    
    Co-authored-by: PilltoPaste42 <5892_sma@kcup.net>
    (cherry picked from commit c543b555368b0d6b36a966f4c803c3fbfbdff918)

---
+++ src/ContactsApp.Models/ProjectManager.cs
@@ -0,0 +1,60 @@
+﻿namespace ContactsApp.Models;
+
+using System.IO;
+
+using Newtonsoft.Json;
+
+/// <summary>
+///     Класс для работы с данными приложения
+/// </summary>
+public sealed class ProjectManager
+{
+    /// <summary>
+    ///     Путь к файлу для чтения/записи данных
+    /// </summary>
+    private const string PROJECT_FILE_PATH = "./ContactsApp.notes";
+
+    /// <summary>
+    ///     Экземпляр менеджера проекта (реализация паттерна Singleton)
+    /// </summary>
+    private static ProjectManager? _instance;
+
+    private ProjectManager()
+    {
+        if (!File.Exists(PROJECT_FILE_PATH))
+        {
+            File.Create(PROJECT_FILE_PATH);
+        }
+    }
+
+    /// <summary>
+    ///     Метод получения экземпляра менеджера проекта
+    /// </summary>
+    public static ProjectManager? Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new ProjectManager();
+
+            return _instance;
+        }
+        private set { }
+    }
+
+    /// <summary>
+    ///     Метод загрузки данных проекта из файла
+    /// </summary>
+    public static Project LoadProject()
+    {
+        var text = File.ReadAllText(PROJECT_FILE_PATH);
+
+        return JsonConvert.DeserializeObject<Project>(text);
+    }
+
+    public static void SaveProject(Project project)
+    {
+        var text = JsonConvert.SerializeObject(project);
+        File.WriteAllText(PROJECT_FILE_PATH, text);
+    }
+}
