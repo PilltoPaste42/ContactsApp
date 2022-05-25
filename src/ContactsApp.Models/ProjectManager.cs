50c87dd928d6105150f7f1fb3f3e81f3c8b91444
PilltoPaste42 <50257225+PilltoPaste42@users.noreply.github.com>
2022-05-26 04:06:33 +0700

    Features/4 add main window logic (#4)
    * Создание решения и проектов
    
    * Создание окон приложения
    
    * Рефакторинг всей реализации
    
    * Исправление вызова новых окон
    
    * Создание структур для реализации бизнес-логики приложения
    
    * Добавление данных для отладки
    
    * Модификация моделей бизнес-логики
    
    * Создание класса фиктивного контакта для отладки
    
    * Создание делигата
    
    * Добавление логики в форму добавления контакта
    
    * Создание логики главного окна
    
    * Обновление представлений
    
    * Clean up всего решения
    
    Co-authored-by: PilltoPaste42 <5892_sma@kcup.net>

--- src/ContactsApp.Models/ProjectManager.cs
+++ src/ContactsApp.Models/ProjectManager.cs
@@ -1,34 +1,33 @@
 ﻿namespace ContactsApp.Models;
 
+using System.Collections.Generic;
 using System.IO;
 
 using Newtonsoft.Json;
 
 /// <summary>
-///     Класс для работы с данными приложения
+///   Класс для работы с данными приложения
 /// </summary>
 public sealed class ProjectManager
 {
     /// <summary>
-    ///     Путь к файлу для чтения/записи данных
+    ///   Путь к файлу для чтения/записи данных
     /// </summary>
     private const string PROJECT_FILE_PATH = "./ContactsApp.notes";
 
     /// <summary>
-    ///     Экземпляр менеджера проекта (реализация паттерна Singleton)
+    ///   Экземпляр менеджера проекта (реализация паттерна Singleton)
     /// </summary>
     private static ProjectManager? _instance;
 
     private ProjectManager()
     {
         if (!File.Exists(PROJECT_FILE_PATH))
-        {
             File.Create(PROJECT_FILE_PATH);
-        }
     }
 
     /// <summary>
-    ///     Метод получения экземпляра менеджера проекта
+    ///   Метод получения экземпляра менеджера проекта
     /// </summary>
     public static ProjectManager? Instance
     {
@@ -43,16 +42,18 @@ public sealed class ProjectManager
     }
 
     /// <summary>
-    ///     Метод загрузки данных проекта из файла
+    ///   Метод загрузки данных проекта из файла
     /// </summary>
-    public static Project LoadProject()
+    public Project LoadProject()
     {
         var text = File.ReadAllText(PROJECT_FILE_PATH);
+        var loadedProject = JsonConvert.DeserializeObject<Project>(text);
 
-        return JsonConvert.DeserializeObject<Project>(text);
+        return loadedProject ?? new Project { Contacts = new List<Contact>() };
     }
 
-    public static void SaveProject(Project project)
+    public void SaveProject(Project project)
+
     {
         var text = JsonConvert.SerializeObject(project);
         File.WriteAllText(PROJECT_FILE_PATH, text);
