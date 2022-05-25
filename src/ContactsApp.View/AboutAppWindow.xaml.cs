83335672caf043953a8e8713850a04d8c457fc0e
PilltoPaste42 <50257225+PilltoPaste42@users.noreply.github.com>
2022-05-26 01:05:27 +0700

    Features/2 add forms layout (#1)
    * Создание решения и проектов
    
    * Создание окон приложения
    
    * Рефакторинг всей реализации
    
    * Исправление вызова новых окон
    
    Co-authored-by: PilltoPaste42 <5892_sma@kcup.net>
    (cherry picked from commit 4897cfbc48a93750548ecf0678b9a51679491755)

---
+++ src/ContactsApp.View/AboutAppWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactsApp.View
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Логика взаимодействия для AboutAppWindow.xaml
+    /// </summary>
+    public partial class AboutAppWindow : Window
+    {
+        public AboutAppWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/ContactsApp.View/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactsApp.View
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
