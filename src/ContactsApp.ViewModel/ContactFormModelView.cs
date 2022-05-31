5588f69a8397b08bde45131ac1c01abe1ea41819
SukharevMA <maxs130872@gmail.com>
2022-06-01 03:03:14 +0700

    temp начало веселья

---
+++ src/ContactsApp.ViewModel/ContactFormModelView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Navigation;
+using ContactsApp.Models;
+
+namespace ContactsApp.ViewModel;
+
+public class ContactFormModelView : INotifyPropertyChanged, IDataErrorInfo
+{
+    private Contact _contact;
+
+    public DateTime Birthday
+    {
+        get
+        {
+            return _contact.Birthday;
+        }
+        set
+        {
+            _contact.Birthday = value;
+            OnPropertyChanged("Birthday");
+        }
+}
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+
+    public string Error => string.Empty;
+    public string this[string columnName] => throw new NotImplementedException();
+    
+}
